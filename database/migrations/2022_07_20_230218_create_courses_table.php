423132c8468dba974d136698e4dcb33546948f3e
jocelinkisenga <kisengajocelin7@gmail.com>
2022-07-21 10:27:29 +0200

    first commit

---
+++ database/migrations/2022_07_20_230218_create_courses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('teacher_id');
+            $table->boolean('comfirmed')->nullable();
+            $table->string('title');
+            $table->text('description');
+            $table->string('language');
+            $table->string('level');
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
